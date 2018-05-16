4ca645473c91be820c403c77224db9a95e3d263b
UnsungGhost <randypweyer@gmail.com>
2018-05-16 10:12:45 -0700

    Add new measure and staves when the notes to match the time signature

--- GuitarTab/wwwroot/js/vextab-input-parse.js
+++ GuitarTab/wwwroot/js/vextab-input-parse.js
@@ -1,16 +1,23 @@
-var wholeNote= ":w "
-var halfNote = ":h "
-var quarterNote = ":q "
-var eighthNote = ":8 "
-var sixteenthNote = ":16 "
-var thirtySecondNote = ":32 "
-var quarterRest = ":4 ## "
-var eighthRest = ":8 ## "
+var wholeNote= ":w ";
+var halfNote = ":h ";
+var quarterNote = ":q ";
+var eighthNote = ":8 ";
+var sixteenthNote = ":16 ";
+var thirtySecondNote = ":32 ";
+var quarterRest = ":4 ## ";
+var eighthRest = ":8 ## ";
+var newMeasure = "| ";
+var newStaff = "\ntabstave time=4/4 key=C notation=true\nnotes ";
 
 var songArr = [];
 var noteType = "";
 var noteFret = "";
 var noteString = "";
+
+var timeSignature=1;
+var noteAmount;
+var measureCount = 2;
+var timeCount = timeSignature;
 
 var TabOutput = "";
 
@@ -27,11 +34,32 @@ function ChooseNoteString(){
   noteString = document.getElementById("guitar-string-selection").value;
   console.log(noteString);
 }
+function CreateNewStaff(){
+  songArr.push(newStaff);
+}
+function CreateNewMeasure(){
+  songArr.push(newMeasure);
+}
 function ConstructNote(){
   finalNote = noteType + noteFret + "/" + noteString + " ";
   songArr.push(finalNote);
+  timeCount -= noteAmount;
+
+  if (timeCount < 0){
+
+    CreateNewMeasure();
+    measureCount -= 1;
+    timeCount=timeSignature;
+  }
+  if (measureCount < 0){
+    CreateNewStaff();
+    measureCount=2;
+    timeCount=timeSignature;
+  }
+
   console.log(finalNote);
 }
+
 function update(newNoteArray){
     $(".editor").val($(".editor").val() + newNoteArray[newNoteArray.length-1]);
     $(function() {
@@ -54,34 +82,42 @@ $(document).ready(function(){
     $("button#whole-note").click(function(event) {
       currentNoteType = wholeNote;
       ChooseNoteType(currentNoteType);
+      noteAmount = 1;
       });
     $("button#half-note").click(function(event) {
       currentNoteType = halfNote;
       ChooseNoteType(currentNoteType);
+      noteAmount = (1/2);
       });
     $("button#quarter-note").click(function(event) {
       currentNoteType = quarterNote;
       ChooseNoteType(currentNoteType);
+      noteAmount = (1/4);
       });
     $("button#eighth-note").click(function(event) {
       currentNoteType = eighthNote;
       ChooseNoteType(currentNoteType);
+      noteAmount = (1/8);
       });
     $("button#sixteenth-note").click(function(event) {
       currentNoteType = sixteenthNote;
       ChooseNoteType(currentNoteType);
+      noteAmount = (1/16);
       });
     $("button#thirty-second-note").click(function(event) {
       currentNoteType = thirtySecondNote;
       ChooseNoteType(currentNoteType);
+      noteAmount = (1/32);
       });
     $("button#quarter-rest").click(function(event) {
       currentNoteType = quarterRest;
       ChooseNoteType(currentNoteType);
+      noteAmount = (1/4);
       });
     $("button#eighth-rest").click(function(event) {
       currentNoteType = eighthRest;
       ChooseNoteType(currentNoteType);
+      noteAmount = (1/8);
       });
 
 
