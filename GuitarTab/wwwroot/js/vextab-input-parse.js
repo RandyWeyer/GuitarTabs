498d021520fed84e9ea9567e0ccd212695a75163
Jimmy McNamara and Kristi Hwang <kristi@>
2018-05-16 16:46:06 -0700

    add multi-line option for adding more than one note to a beat

--- GuitarTab/wwwroot/js/vextab-input-parse.js
+++ GuitarTab/wwwroot/js/vextab-input-parse.js
@@ -136,4 +136,67 @@ $(document).ready(function(){
             $("#final-tab-output").text("1");
 
           });
+          $("button#add-multiple-notes").click(function() {
+            $("button#add-multiple-notes").hide();
+            $("#single-notes").append("<div id='many-notes'>" +
+                                        "<label>How Many Notes Are You Adding:</label>" +
+                                        "<select id='guitar-notes-selection' name='guitar-notes-selection' type='text'>" +
+                                           "<option value=1>1</option>" +
+                                           "<option value=2>2</option>" +
+                                           "<option value=3>3</option>" +
+                                           "<option value=4>4</option>" +
+                                           "<option value=5>5</option>" +
+                                           "<option value=6>6</option>" +
+                                        "</select>" +
+                                        "<button type='button' class='btn btn-primary' id='select-number-of-strings'>Go</button>" +
+                                      "</div>");
+          function multipleNotes(num) {
+            for(var x = 0; x<num; x++) {
+              $("#single-notes").prepend("<label>Fret:</label>" +
+                                            "<select onchange='ChooseNoteFret()' class='multiple-guitar-fret-selection' name='multiple-guitar-fret-selection' type='text'>" +
+                                              "<option value=0>0</option>" +
+                                              "<option value=1>1</option>" +
+                                              "<option value=2>2</option>" +
+                                              "<option value=3>3</option>" +
+                                              "<option value=4>4</option>" +
+                                              "<option value=5>5</option>" +
+                                              "<option value=6>6</option>" +
+                                              "<option value=7>7</option>" +
+                                              "<option value=8>8</option>" +
+                                              "<option value=9>9</option>" +
+                                              "<option value=10>10</option>" +
+                                              "<option value=11>11</option>" +
+                                              "<option value=12>12</option>" +
+                                              "<option value=13>13</option>" +
+                                              "<option value=14>14</option>" +
+                                              "<option value=15>15</option>" +
+                                              "<option value=16>16</option>" +
+                                              "<option value=17>17</option>" +
+                                              "<option value=18>18</option>" +
+                                              "<option value=19>19</option>" +
+                                              "<option value=20>20</option>" +
+                                              "<option value=21>21</option>" +
+                                              "<option value=22>22</option>" +
+                                              "<option value=23>23</option>" +
+                                              "<option value=24>24</option>" +
+                                            "</select>" +
+                                          "<label>String:</label>" +
+                                            "<select onchange='ChooseNoteString()' class='multiple-guitar-string-selection' name='multiple-guitar-string-selection' type='text'>" +
+                                               "<option value=1>1</option>" +
+                                               "<option value=2>2</option>" +
+                                               "<option value=3>3</option>" +
+                                               "<option value=4>4</option>" +
+                                               "<option value=5>5</option>" +
+                                               "<option value=6>6</option>" +
+                                            "</select>" +
+                                            "<br>");
+            }
+          };
+
+          $("button#select-number-of-strings").click(function() {
+            $("#many-notes").hide();
+            multipleNotes($("#guitar-notes-selection").val());
+          });
+        });
+
 });
