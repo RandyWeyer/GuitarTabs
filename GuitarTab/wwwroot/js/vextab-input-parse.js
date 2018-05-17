7e71e2c1dfbbe274cf2447b7842849dcd13d5f22
Jimmy McNamara and Kristi Hwang <kristi@>
2018-05-17 12:09:11 -0700

    let the user select multiple notes for the same beat and display them on the staff.

--- GuitarTab/wwwroot/js/vextab-input-parse.js
+++ GuitarTab/wwwroot/js/vextab-input-parse.js
@@ -18,6 +18,7 @@ var timeSignature=1;
 var noteAmount;
 var measureCount = 2;
 var timeCount = timeSignature;
+var finalNote;
 
 var TabOutput = "";
 
@@ -27,11 +28,11 @@ function ChooseNoteType(currentNoteType){
   console.log(noteType);
 }
 function ChooseNoteFret(){
-  noteFret = document.getElementById("guitar-fret-selection").value;
+  noteFret = document.getElementById("guitar-fret-selection-0").value;
   console.log(noteFret);
 }
 function ChooseNoteString(){
-  noteString = document.getElementById("guitar-string-selection").value;
+  noteString = document.getElementById("guitar-string-selection-0").value;
   console.log(noteString);
 }
 function CreateNewStaff(){
@@ -42,23 +43,65 @@ function CreateNewMeasure(){
 }
 function ConstructNote(){
   finalNote = noteType + noteFret + "/" + noteString + " ";
-  songArr.push(finalNote);
-  timeCount -= noteAmount;
 
-  if (timeCount < 0){
-
-    CreateNewMeasure();
-    measureCount -= 1;
-    timeCount=timeSignature;
-  }
-  if (measureCount < 0){
-    CreateNewStaff();
-    measureCount=2;
-    timeCount=timeSignature;
-  }
-
-  console.log(finalNote);
+  // songArr.push(finalNote);  moved this down after the if statement in the add note button
+  // timeCount -= noteAmount;
+  //
+  // if (timeCount < 0){
+  //
+  //   CreateNewMeasure();
+  //   measureCount -= 1;
+  //   timeCount=timeSignature;
+  // }
+  // if (measureCount < 0){
+  //   CreateNewStaff();
+  //   measureCount=2;
+  //   timeCount=timeSignature;
+  // }
+  //
+  // console.log(finalNote);
 }
+
+function ConstructMultipleNotes() {
+  console.log(noteType);
+  finalNote = (noteType + "(");
+  // var multArray = [];
+  for(var i = 0; i<=$("#guitar-notes-selection").val(); i++) {
+    noteFreti = $("#guitar-fret-selection-"+i).val();
+    noteStri = $("#guitar-string-selection-"+i).val();
+    // multArray.push(noteStri + "/" + noteFreti);
+    finalNote += noteFreti + "/" + noteStri;
+    console.log(finalNote);
+    if (i == $("#guitar-notes-selection").val()) {
+      finalNote += ") ";
+    } else {
+      finalNote += ".";
+    }
+  }
+
+  //JOHNS kinda weird suggestion to work from
+  // var noteToStore = noteType + ": " + noteFreti + "/" + noteStri
+  // if (i = finalNoteNum) {
+  //   noteToStore += ")";
+  // } else {
+  //   noteToStore += ".";
+  // }
+  // tempArr.push(noteToStore);
+
+  // tempString = tempArr.toString();
+  // splitString = tempString.split("");
+  // removePeriod = splitString.pop();
+  // addParens = splitString.push(")");
+  // backToString = addParens.toString();
+  // finalNote = backToString.replace(/,/g,"");
+};
+
+function test()
+{
+
+  return false;
+}
+
 
 function update(newNoteArray){
     $(".editor").val($(".editor").val() + newNoteArray[newNoteArray.length-1]);
@@ -122,10 +165,31 @@ $(document).ready(function(){
 
 
         $("button#add-note").click(function(event) {
+            console.log($("#guitar-fret-selection-1").val());
             console.log(noteType);
             console.log(noteFret);
-            console.log(noteString);
-            ConstructNote();
+            console.log();
+            if ($("#guitar-notes-selection").val()>0) {
+              ConstructMultipleNotes();
+            } else {
+              ConstructNote();
+            }
+            console.log(finalNote);
+            songArr.push(finalNote);
+            timeCount -= noteAmount;
+
+            if (timeCount < 0){
+
+              CreateNewMeasure();
+              measureCount -= 1;
+              timeCount=timeSignature;
+            }
+            if (measureCount < 0){
+              CreateNewStaff();
+              measureCount=2;
+              timeCount=timeSignature;
+            }
+
 
             update(songArr);
 
@@ -136,60 +200,60 @@ $(document).ready(function(){
             $("#final-tab-output").text("1");
 
           });
+
           $("button#add-multiple-notes").click(function() {
             $("button#add-multiple-notes").hide();
             $("#single-notes").append("<div id='many-notes'>" +
-                                        "<label>How Many Notes Are You Adding:</label>" +
+                                        "<label>Number of Additional Notes:</label>" +
                                         "<select id='guitar-notes-selection' name='guitar-notes-selection' type='text'>" +
                                            "<option value=1>1</option>" +
                                            "<option value=2>2</option>" +
                                            "<option value=3>3</option>" +
                                            "<option value=4>4</option>" +
                                            "<option value=5>5</option>" +
-                                           "<option value=6>6</option>" +
                                         "</select>" +
                                         "<button type='button' class='btn btn-primary' id='select-number-of-strings'>Go</button>" +
                                       "</div>");
           function multipleNotes(num) {
             for(var x = 0; x<num; x++) {
-              $("#single-notes").prepend("<label>Fret:</label>" +
-                                            "<select onchange='ChooseNoteFret()' class='multiple-guitar-fret-selection' name='multiple-guitar-fret-selection' type='text'>" +
-                                              "<option value=0>0</option>" +
-                                              "<option value=1>1</option>" +
-                                              "<option value=2>2</option>" +
-                                              "<option value=3>3</option>" +
-                                              "<option value=4>4</option>" +
-                                              "<option value=5>5</option>" +
-                                              "<option value=6>6</option>" +
-                                              "<option value=7>7</option>" +
-                                              "<option value=8>8</option>" +
-                                              "<option value=9>9</option>" +
-                                              "<option value=10>10</option>" +
-                                              "<option value=11>11</option>" +
-                                              "<option value=12>12</option>" +
-                                              "<option value=13>13</option>" +
-                                              "<option value=14>14</option>" +
-                                              "<option value=15>15</option>" +
-                                              "<option value=16>16</option>" +
-                                              "<option value=17>17</option>" +
-                                              "<option value=18>18</option>" +
-                                              "<option value=19>19</option>" +
-                                              "<option value=20>20</option>" +
-                                              "<option value=21>21</option>" +
-                                              "<option value=22>22</option>" +
-                                              "<option value=23>23</option>" +
-                                              "<option value=24>24</option>" +
-                                            "</select>" +
-                                          "<label>String:</label>" +
-                                            "<select onchange='ChooseNoteString()' class='multiple-guitar-string-selection' name='multiple-guitar-string-selection' type='text'>" +
-                                               "<option value=1>1</option>" +
-                                               "<option value=2>2</option>" +
-                                               "<option value=3>3</option>" +
-                                               "<option value=4>4</option>" +
-                                               "<option value=5>5</option>" +
-                                               "<option value=6>6</option>" +
-                                            "</select>" +
-                                            "<br>");
+              $("#single-notes").prepend("<label>#" + (x+1) + ". Fret:</label>" +
+                                          "<select onchange='ChooseNoteFret()' id='guitar-fret-selection-" + (x+1) + "' name='guitar-fret-selection-" + (x+1) + "' type='text'>" +
+                                            "<option value=0>0</option>" +
+                                            "<option value=1>1</option>" +
+                                            "<option value=2>2</option>" +
+                                            "<option value=3>3</option>" +
+                                            "<option value=4>4</option>" +
+                                            "<option value=5>5</option>" +
+                                            "<option value=6>6</option>" +
+                                            "<option value=7>7</option>" +
+                                            "<option value=8>8</option>" +
+                                            "<option value=9>9</option>" +
+                                            "<option value=10>10</option>" +
+                                            "<option value=11>11</option>" +
+                                            "<option value=12>12</option>" +
+                                            "<option value=13>13</option>" +
+                                            "<option value=14>14</option>" +
+                                            "<option value=15>15</option>" +
+                                            "<option value=16>16</option>" +
+                                            "<option value=17>17</option>" +
+                                            "<option value=18>18</option>" +
+                                            "<option value=19>19</option>" +
+                                            "<option value=20>20</option>" +
+                                            "<option value=21>21</option>" +
+                                            "<option value=22>22</option>" +
+                                            "<option value=23>23</option>" +
+                                            "<option value=24>24</option>" +
+                                          "</select>" +
+                                        "<label>String:</label>" +
+                                          "<select onchange='ChooseNoteString()' id='guitar-string-selection-" + (x+1) + "' name='guitar-string-selection-" + (x+1) + "' type='text'>" +
+                                             "<option value=1>1</option>" +
+                                             "<option value=2>2</option>" +
+                                             "<option value=3>3</option>" +
+                                             "<option value=4>4</option>" +
+                                             "<option value=5>5</option>" +
+                                             "<option value=6>6</option>" +
+                                          "</select>" +
+                                          "<br>");
             }
           };
 
